c02ab110d6e7d6476aa09ccda79d1a1a45e9c0fa
blademainer <blademainer@gmail.com>
2026-07-13 11:34:38 +0800

    Avoid activation policy switch for region overlay

--- MacScreenCapture/Core/CaptureManager.swift
+++ MacScreenCapture/Core/CaptureManager.swift
@@ -1806,8 +1806,6 @@ class CaptureManager: ObservableObject {
             var didResume = false
             var selectionWindows: [NSWindow] = []
 
-            NSApp.setActivationPolicy(.regular)
-
             let completeSelection: (Result<CGRect, Error>) -> Void = { result in
                 guard !didResume else { return }
                 didResume = true
@@ -1821,9 +1819,6 @@ class CaptureManager: ObservableObject {
                     }
                     self?.activeRegionSelectionWindows.removeAll { activeWindow in
                         selectionWindows.contains { $0 === activeWindow }
-                    }
-                    if WindowManager.shared.isMainWindowVisible == false {
-                        NSApp.setActivationPolicy(.accessory)
                     }
                 }
             }
